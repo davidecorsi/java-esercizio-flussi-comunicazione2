fb84a1bfbd697005f1076a274557427fa2d75728
Davide Corsi <davide.corsi@par-tec.it>
2021-09-13 10:49:44 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * L'applicazione deve copiare un file. Il file di destinazione deve essere fornito dall'utente. Nel caso il
+ * file di destinazione già esiste, dovrà essere chiesto all'utente se vuole modificare il nome. Se l'utente
+ * non lo vuole modificare il file di destinazione viene cancellato e ricreato con il nuovo contenuto.
+ * Il file sorgente viene fornito tramite argomento nel lancio dell'applicazione, mentre il file di
+ * destinazione viene richiesto all'utente una volta che l'applicazione è in esecuzione.
+ */
+public class Main {
+
+}
