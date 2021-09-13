5d60e7f6001a507642172a09956ce523439ec2af
Davide Corsi <davide.corsi@par-tec.it>
2021-09-13 10:50:42 +0200

    Risoluzione

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * L'applicazione deve copiare un file. Il file di destinazione deve essere fornito dall'utente. Nel caso il
  * file di destinazione già esiste, dovrà essere chiesto all'utente se vuole modificare il nome. Se l'utente
@@ -6,5 +14,34 @@
  * destinazione viene richiesto all'utente una volta che l'applicazione è in esecuzione.
  */
 public class Main {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Inserire il nome del file di destinazione ");
+		File destinationFile = new File(input.nextLine());
+		if(destinationFile.exists()) {
+			int choose;
+			do {
+				System.out.println("Il file di destinazione già esiste, vuoi cambiarlo\n0 - Si\n1- No");
+				choose = input.nextInt();
+				input.nextLine();
+			} while(choose != 0 && choose != 1);
+			if(choose == 0) {
+				System.out.print("Inserire il nuovo nome del file ");
+				destinationFile = new File(input.nextLine());
+				
+			} else {
+				destinationFile.delete();
+			}
+		}
+		try(BufferedReader brd = new BufferedReader(new FileReader(args[0]));
+			BufferedWriter bwt = new BufferedWriter(new FileWriter(destinationFile))) {
+			String line;
+			while((line = brd.readLine()) != null) {
+				bwt.write(line);
+				bwt.newLine();
+			}
+		} catch(IOException e) {
+			System.out.println("Errore nella lettura del file " + e);
+		}
+	}
 }
